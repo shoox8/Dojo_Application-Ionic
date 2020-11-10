57dbc195bc99bbfb18795163ed592ed037ac39e5
Fco. Javier Cordoba Rey <i72coref@uco.es>
2020-11-11 00:33:11 +0100

    Fallo ruta arreglado

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -12,10 +12,6 @@ const routes: Routes = [
         loadChildren: () => import('../home/home.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-      },
-      {
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
