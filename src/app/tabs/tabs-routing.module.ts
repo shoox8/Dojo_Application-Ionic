8ad531a44a701de30a60802594187e51bebf8141
shoox8 <shox99@protonmail.ch>
2020-11-11 17:12:04 +0100

    Modificando rutas

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -13,11 +13,11 @@ const routes: Routes = [
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () => import('../nfc/nfc.module').then(m => m.Tab2PageModule)
       },
       {
         path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        loadChildren: () => import('../reto/reto.module').then(m => m.Tab3PageModule)
       },
       {
         path: '',
