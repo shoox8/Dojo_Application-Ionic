419d5cbdd51c772b5fabc6d0caa5fd0995728b42
shoox8 <shox99@protonmail.ch>
2020-11-11 22:51:43 +0100

    Login Fronted (terminado...quedaria cambiar el fondo)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
